105848374f4b50b78b735cbe0d469598ec2afc0c
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2024-02-08 14:19:58 +0530

    admin routes done

--- routes/admin.js
+++ routes/admin.js
@@ -12,14 +12,14 @@ const {
   getAllJuniorAdmins,
 } = require("../controllers/admin");
 
-adminRouter.get("/users", getAllUsers);
-adminRouter.get("/users/:id", getUserById);
-adminRouter.get("/executives", getAllExecutives);
-adminRouter.get("/junior-admins", getAllJuniorAdmins);
-adminRouter.post("/make-junior-admin/:id", makeJuniorAdmin);
-adminRouter.post("/make-executive-admin/:id", makeExecutiveAdmin);
-adminRouter.post("/remove-admin/:id", removeAdmin);
-adminRouter.put("/edit-profile/:id", editProfile);
-adminRouter.delete("/delete-user/:id", deleteUser);
+adminRouter.get("/users", getAllUsers); // done
+adminRouter.get("/users/:id", getUserById); // done
+adminRouter.get("/executives", getAllExecutives); // done
+adminRouter.get("/junior-admins", getAllJuniorAdmins); // done
+adminRouter.post("/make-junior-admin/:id", makeJuniorAdmin); // done
+adminRouter.post("/make-executive-admin/:id", makeExecutiveAdmin); // done
+adminRouter.post("/remove-admin/:id", removeAdmin); // done
+adminRouter.put("/edit-profile/:id", editProfile); // done
+adminRouter.delete("/delete-user/:id", deleteUser); // done
 
 module.exports = adminRouter;
